e303a2fa664eb487968eb368118957b086f3beab
kmlnrao2008 <kmlnrao2008@gmail.com>
2023-05-02 13:48:52 +0530

    first commit

---
+++ index.js
@@ -0,0 +1,16 @@
+var express = require('express');
+var app = express();
+
+app.get('/', function(req, res){
+   res.send("Hello world!");
+});
+
+app.get('/hello', function(req, res){
+    res.send("Hello World!");
+ });
+ 
+app.get('/:id', function(req, res){
+    res.send('The id you specified is ' + req.params.id);
+ });
+
+app.listen(3000);
